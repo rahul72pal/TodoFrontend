9ad221c8d832d70bac1848354f120cc439e86f3d
Rahul <rahulgwl72@gmail.com>
2024-01-03 22:37:21 +0530

    api

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const axiosInstance = axios.create({});
 
 export const apiConnector = (methode, url , bodyData, headers, params)=>{
-    console.log(methode, url , bodyData, headers, params);
+    console.log("API Connector =",methode, url , bodyData, headers, params);
     console.log("data 3",bodyData);
     return axiosInstance({
         method: `${methode}`,
